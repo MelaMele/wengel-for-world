ff66769b0cd27f8db8d32aaa7b83adb2b2987f12
MelaMele <melamele06@gmail.com>
2026-09-13 13:20:10 +0300

    Refactor pastor routes to use PastorController

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PastorController;
 
+// ዋና መነሻ ገጽ
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+// የፀሎት ጥያቄዎች
 Route::get('/prayer-requests', [HomeController::class, 'prayerIndex'])->name('prayer.index');
 Route::post('/prayer-requests', [HomeController::class, 'prayerStore'])->name('prayer.store');
 
-Route::get('/teachings', function () {
-    $teachings = \App\Models\Teaching::latest()->paginate(12);
-    return view('teachings.index', compact('teachings'));
-})->name('teachings.index');
-
-Route::get('/pastors', function () {
-    $pastors = \App\Models\User::where('role', 'pastor')->get();
-    return view('pastors.index', compact('pastors'));
-})->name('pastors.index');
+// የፓስተሮች አገልግሎት እና ሚስጥራዊ ምክር
+Route::get('/pastors', [PastorController::class, 'index'])->name('pastors.index');
+Route::get('/pastors/{id}', [PastorController::class, 'show'])->name('pastors.show');
+Route::post('/pastors/{id}/counseling', [PastorController::class, 'sendCounseling'])->name('pastors.counseling');
